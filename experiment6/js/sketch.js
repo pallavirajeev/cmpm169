5841f17f6a407cfe9f5abbe3511e208c78774b65
Pallavi Rajeev <pallavirajeev44@gmail.com>
2025-02-17 21:24:59 -0800

    adding in exp 6

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,79 +1,148 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Blackout Poetry Interactive Visualization
+// Author: Pallavi Rajeev
+// Date: February 17, 2025
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+// Constants
+const FONT_SIZE = 25;
+const TEXT_COLOR = 255;
+const HIDDEN_TEXT_COLOR = "rgb(111,111,111)";
+const BACKGROUND_COLOR = "rgb(239,138,222)";
+const MARGIN = 50;
+const LINE_HEIGHT = 30;
+const TEXT_Y_START = 300;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+// Original scary texts and hidden romantic texts
+const ORIGINAL_TEXTS = [
+    "the door slowly creaked open and revealed a figure hidden within the darkest parts of the shadows",
+    "a quiet tapping interrupted the eery silence but upon first look there seemed to be nothing there",
+    "a trembling hand reached forward as moonlight revealed a blood-splattered canvas with stench thick in the air",
+];
+
+const HIDDEN_TEXTS = [
+    "the way you look at me warms my body during the cool summer nights we spend together",
+    "the temperature of your breath against me contrasts the windy breezes of the beaches we lay on",
+    "your soft thumbs linger along the corners of my saddened eyes wiping away all feelings of sorrow",
+];
 
 // Globals
-let myInstance;
 let canvasContainer;
-var centerHorz, centerVert;
+let blackoutPoem;
+let centerHorz, centerVert;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+class BlackoutPoem {
+    constructor() {
+        this.reset();
     }
 
-    myMethod() {
-        // code to run when method is called
+    reset() {
+        let randomIndex = floor(random(ORIGINAL_TEXTS.length));
+        this.wordsOriginal = ORIGINAL_TEXTS[randomIndex].split(" ");
+        this.wordsHidden = HIDDEN_TEXTS[randomIndex].split(" ");
+        this.blackout = new Array(this.wordsOriginal.length).fill(false);
+        this.blackoutHistory = new Array(this.wordsOriginal.length).fill(false);
+        this.isHiddenWord = new Array(this.wordsOriginal.length).fill(false);
+    }
+
+    draw() {
+        background(BACKGROUND_COLOR);
+        textSize(FONT_SIZE);
+        
+        let x = MARGIN;
+        let y = TEXT_Y_START;
+        
+        for (let i = 0; i < this.wordsOriginal.length; i++) {
+            let word = this.wordsOriginal[i];
+            
+            if (this.blackout[i]) {
+                fill(0);
+                rect(x - 2, y - 18, textWidth(word) + 4, 22);
+            } else {
+                fill(this.isHiddenWord[i] ? HIDDEN_TEXT_COLOR : TEXT_COLOR);
+                text(word, x, y);
+            }
+            
+            x += textWidth(word) + 10;
+            if (x > width - MARGIN * 2) {
+                x = MARGIN;
+                y += LINE_HEIGHT;
+            }
+        }
+    }
+
+    handleClick(mouseX, mouseY) {
+        let x = MARGIN;
+        let y = TEXT_Y_START;
+        
+        for (let i = 0; i < this.wordsOriginal.length; i++) {
+            let word = this.wordsOriginal[i];
+            let w = textWidth(word) + 10;
+            
+            if (mouseX > x - 2 && mouseX < x + w && mouseY > y - 18 && mouseY < y + 4) {
+                this.blackout[i] = !this.blackout[i];
+                this.blackoutHistory[i] = this.blackout[i];
+            }
+            
+            x += w;
+            if (x > width - MARGIN * 2) {
+                x = MARGIN;
+                y += LINE_HEIGHT;
+            }
+        }
+    }
+
+    revealHidden() {
+        for (let i = 0; i < this.blackout.length; i++) {
+            if (this.blackout[i]) {
+                this.wordsOriginal[i] = this.wordsHidden[i];
+                this.isHiddenWord[i] = true;
+                this.blackout[i] = false;
+            }
+        }
+    }
+
+    restoreBlackouts() {
+        for (let i = 0; i < this.blackoutHistory.length; i++) {
+            if (this.blackoutHistory[i]) {
+                this.blackout[i] = true;
+            }
+        }
     }
 }
 
 function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+    centerHorz = canvasContainer.width() / 2;
+    centerVert = canvasContainer.height() / 2;
+    console.log("Resizing...");
+    resizeCanvas(canvasContainer.width(), canvasContainer.height());
 }
 
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
+    canvasContainer = $("#canvas-container");
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    canvas.parent("canvas-container");
+    
+    blackoutPoem = new BlackoutPoem();
+    
+    $(window).resize(function() {
+        resizeScreen();
+    });
     resizeScreen();
-  });
-  resizeScreen();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+    blackoutPoem.draw();
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+    blackoutPoem.handleClick(mouseX, mouseY);
+}
+
+function keyPressed() {
+    if (key === ' ') {
+        blackoutPoem.revealHidden();
+    } else if (key === 'B' || key === 'b') {
+        blackoutPoem.restoreBlackouts();
+    } else if (key === 'R' || key === 'r') {
+        blackoutPoem.reset();
+    }
 }
